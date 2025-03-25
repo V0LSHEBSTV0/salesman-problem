1e0c165a3f5932ba7584b285a5467d1e7fdb8264
bowie <bolshebstvo>
2025-03-26 01:02:17 +0500

    init

---
+++ main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void* read_from_file(char* file)
+{
+    
+    return
+}
+
+int main() {
+    
+    char input_file[] = "input.txt";
+
+    int size = sizeof(int *);
+    printf("%d\n", size );
+
+    return 0;
+}
